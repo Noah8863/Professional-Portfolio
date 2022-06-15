1a792fb908dff3633592609fb2a95f44951d6a61
Noah8863 <stacknoah99@gmail.com>
2022-06-14 18:14:27 -0600

    restyled the page and created zones. Made flip cards for first section

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+
+//Functions for displaying/hiding information about past experince
+function bestbuy(){
+    const bestbuyinfo = document.getElementById('bestbuyinfo')
+    bestbuyinfo.classList.toggle('hide')
+}
+function observ(){
+    const observinfo = document.getElementById('observinfo')
+    observinfo.classList.toggle('hide')
+}
+function verizon (){
+    const verizoninfo = document.getElementById('verizoninfo')
+    verizoninfo.classList.toggle('hide')
+}
